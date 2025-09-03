ee8390f1e1ea78c49f3f2acfe0b90dbc87d98447
Velyaora <jinalk620@gmail.com>
2025-09-03 22:03:26 +0000

    Update send.js
    Signed-off-by: Velyaora <jinalk620@gmail.com>

--- api/send.js
+++ api/send.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
   }
 
   const { message } = req.body;
-  const TELEGRAM_BOT_TOKEN = "7569416193:AAF8Nr7RWGGuhjhUkWrR-oFlDWaiYEVQBmM";
+  const TELEGRAM_BOT_TOKEN = "6961886563:AAHZwl-UaAWaGgXwzyp1vazRu1Hf37FKX2A";
   const CHAT_ID = "-1002290156309";
 
   try {
